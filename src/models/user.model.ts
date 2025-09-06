823973167f40e08d7cb8244d84e57a9e31df22d3
Assis-Mohanty <assis.mohanty.98@gmail.com>
2025-09-06 19:54:38 +0530

    added users model

---
+++ src/models/user.model.ts
@@ -0,0 +1,53 @@
+import {Model, InferAttributes,InferCreationAttributes,CreationOptional, DataTypes} from "sequelize"
+import sequelize from "./sequelize";
+
+class User extends Model<InferAttributes<User>,InferCreationAttributes<User>>{
+    declare id: CreationOptional<number>;
+    declare name:string;
+    declare email:string;
+    declare passwordHash:string;
+    declare isAdmin:Boolean|false;
+    declare createdAt:CreationOptional<Date>;
+    declare updatedAt:CreationOptional<Date>;
+}
+
+User.init({
+    id:{
+        type:"INTEGER",
+        autoIncrement:true,
+        primaryKey:true
+    },
+    name:{
+        type:"STRING",
+        allowNull:false
+    },
+    email:{
+        type:"STRING",
+        allowNull:false
+    },
+    isAdmin:{
+        type:"BOOLEAN",
+        allowNull:false,
+        defaultValue:false
+    },
+    passwordHash:{
+        type:"STRING",
+        allowNull:false
+    },
+    createdAt:{
+        type:"DATE",
+        allowNull:false,
+    },
+    updatedAt:{
+        type:"DATE",
+        allowNull:false,
+    },
+    
+},{
+    sequelize:sequelize,
+    tableName:'users',
+    underscored:true,
+    paranoid:true,
+})
+
+export default User;
